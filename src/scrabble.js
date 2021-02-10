56e3455fa7c01b30641845708432d458252a6927
Samantha <samanthagottlieb31@gmail.com>
2021-02-10 14:35:46 +0000

    Calculate score correctly

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,10 +1,24 @@
 class Scrabble {
-  calculateScore(letter) {
-    if (letter === "A") {
-      return 1;
-    } else {
-      return 2;
-    }
+  calculateScore(letters) {
+    let score = 0;
+    let lettersArray = letters.split("");
+
+    lettersArray.forEach((letter) => {
+      let letterPoints = this.calculatePoints(letter);
+      score += letterPoints;
+    });
+
+    return score;
+  }
+
+  calculatePoints(letter) {
+    const points = {
+      A: 1,
+      B: 3,
+      T: 1,
+    };
+
+    return points[letter];
   }
 }
 
--- test/scrabble.test.js
+++ test/scrabble.test.js
@@ -5,5 +5,7 @@ describe("Scrabble", () => {
     let game = new Scrabble();
     expect(game.calculateScore("A")).toEqual(1);
     expect(game.calculateScore("AT")).toEqual(2);
+    expect(game.calculateScore("BAT")).toEqual(5);
+    expect(game.calculateScore("GUARDIAN")).toEqual(5);
   });
 });
