47893a76a4c3875136783c563c306d19cbe7f209
Samantha <samanthagottlieb31@gmail.com>
2021-02-10 14:40:50 +0000

    Fix testing error

--- src/scrabble.js
+++ src/scrabble.js
@@ -16,6 +16,12 @@ class Scrabble {
       A: 1,
       B: 3,
       T: 1,
+      G: 2,
+      U: 1,
+      R: 1,
+      D: 2,
+      I: 1,
+      N: 1,
     };
 
     return points[letter];
--- test/scrabble.test.js
+++ test/scrabble.test.js
@@ -6,6 +6,6 @@ describe("Scrabble", () => {
     expect(game.calculateScore("A")).toEqual(1);
     expect(game.calculateScore("AT")).toEqual(2);
     expect(game.calculateScore("BAT")).toEqual(5);
-    expect(game.calculateScore("GUARDIAN")).toEqual(5);
+    expect(game.calculateScore("GUARDIAN")).toEqual(10);
   });
 });
